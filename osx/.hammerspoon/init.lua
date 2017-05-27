7f2a36b180a04ce88259da3e593138c4003cfa67
Jonathan Van Eenwyk <jvaneenwyk@tendrilinc.com>
2017-05-26 18:49:38 -0600

    Switch to hammerspoon

---
+++ osx/.hammerspoon/init.lua
@@ -0,0 +1,26 @@
+-- HANDLE SCROLLING
+local oldmousepos = {}
+local scrollmult = 4   -- negative multiplier makes mouse work like traditional scrollwheel
+
+mousetap = hs.eventtap.new({5}, function(e)
+   oldmousepos = hs.mouse.getAbsolutePosition()
+   local mods = hs.eventtap.checkKeyboardModifiers()
+
+   if mods['ctrl'] and mods['cmd'] then
+      -- print ("will scroll")
+      local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+      local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+      local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},'pixel')
+      scroll:post()
+
+      -- put the mouse back
+      hs.mouse.setAbsolutePosition(oldmousepos)
+
+      return true
+   else
+      return false, {}
+   end
+end)
+
+mousetap:start()
+
