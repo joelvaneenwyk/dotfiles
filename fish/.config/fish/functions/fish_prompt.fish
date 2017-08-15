944a47a3a5087eb008138544da784bc0424bcf06
Jonathan Van Eenwyk <jvaneenwyk@tendrilinc.com>
2017-08-15 14:40:40 -0600

    Clean up fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,16 +1,20 @@
 function fish_prompt --description 'Write out the prompt'
-   
+
    set -l last_status $status
 
    # Just calculate these once, to save a few cycles when displaying the prompt
-   if not set -q __fish_prompt_hostname
-      set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+   if not set -q __fish_prompt_host
+      if test -n "$SSH_CONNECTION"
+         set -g __fish_prompt_host (set_color $fish_color_user) "$USER" (set_color normal) @ (set_color $fish_color_host) (hostname|cut -d . -f 1) (set_color normal) ' '
+      else
+         set -g __fish_prompt_host ""
+      end
    end
 
    if not set -q __fish_prompt_normal
       set -g __fish_prompt_normal (set_color normal)
    end
-   
+
    if not set -q -g __fish_classic_git_functions_defined
       set -g __fish_classic_git_functions_defined
 
@@ -20,14 +24,14 @@ function fish_prompt --description 'Write out the prompt'
             commandline -f repaint ^/dev/null
          end
       end
-      
+
       function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
          if status --is-interactive
             set -e __fish_prompt_host
             commandline -f repaint ^/dev/null
          end
       end
-      
+
       function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
          if status --is-interactive
             set -e __fish_prompt_status
@@ -66,14 +70,7 @@ function fish_prompt --description 'Write out the prompt'
       set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
    end
 
-   if not set -q __fish_prompt_user
-      set -g __fish_prompt_user (set_color $fish_color_user)
-   end
-   if not set -q __fish_prompt_host
-      set -g __fish_prompt_host (set_color $fish_color_host)
-   end
-
-   echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+   echo -n -s "$__fish_prompt_host" "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
 
 if not set -q __prompt_initialized_2
