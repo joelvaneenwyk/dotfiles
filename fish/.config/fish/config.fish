ecf730eda3ca7788331965ab74673e7975c5926e
Jonathan Van Eenwyk <jvaneenwyk@tendrilinc.com>
2017-11-07 11:07:44 -0700

    Import fish_command_timer directly
    fundle can't seem to load this plugin.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,6 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'edc/bass'
 fundle plugin 'fisherman/fzf'
 fundle plugin 'fisherman/z'
-fundle plugin 'jichu4n/fish-command-timer'
 fundle init
 
 # suppress fish greeting
