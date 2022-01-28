7575801bbc4f73d9b3a8e05b4694f06690a4321b
Jonathan Van Eenwyk <hello@jdve.me>
2022-01-28 09:16:19 -0800

    Simplify keyboard layout switching

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,15 +1,15 @@
 usbWatcher = nil
-keyboardLayout = nil
+keyboardLayout = hs.keycodes.currentLayout()
 
 -- Switch keyboard layout when Moonlander is added/removed.
 function usbDeviceCallback(data)
    if (data["productName"] == "Moonlander Mark I") then
       if (data["eventType"] == "added") then
          keyboardLayout = "U.S."
-         hs.keycodes.setLayout("U.S.")
+         hs.keycodes.setLayout(keyboardLayout)
       elseif (data["eventType"] == "removed") then
          keyboardLayout = "Norman"
-         hs.keycodes.setLayout("Norman")
+         hs.keycodes.setLayout(keyboardLayout)
       end
    end
 end
@@ -18,23 +18,4 @@ usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
 
 hyper = {"ctrl", "alt", "shift", "command"}
-
--- Add global hotkeys to switch keyboard layout.
-function setLayout(name)
-   return function()
-      hs.keycodes.setLayout(name)
-   end
-end
-
-hs.hotkey.bind(hyper, "n", setLayout("Norman"))
-hs.hotkey.bind(hyper, "i", setLayout("U.S."))
-
--- Add global hotkey to switch to Hindi input. This requires making
--- sure that keyboard layout matches attached keyboard layout.
-function setHindi()
-   hs.keycodes.setLayout(keyboardLayout)
-   hs.keycodes.setMethod("Hindi (A→अ)")
-end
-
-hs.hotkey.bind(hyper, "o", setHindi)
-
+meh = {"ctrl", "alt", "shift"}
