dbaeb3d37ba1c8963284f6866e651cd88187c2ea
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-28 18:59:21 -0800

    Add settings for Sublime

--- source/windows/bin/stow.bat
+++ source/windows/bin/stow.bat
@@ -1,9 +1,11 @@
 @echo off
 
-setlocal EnableDelayedExpansion
+setlocal EnableDelayedExpansion EnableExtensions
 
+set _env=%CD%
+cd /d "%MYCELIO_ROOT%\packages"
 call "%~dp0env.bat"
-
 call "%~dp0run.bat" "%PERL%" -I "%MYCELIO_ROOT%\source\stow\lib" ^
     "%MYCELIO_ROOT%\source\stow\bin\stow" ^
     --dir="%MYCELIO_ROOT%\packages" --target="%USERPROFILE%" --verbose %*
+cd /d "%_env%"
