9166825b347d7bc5e59c0b69a2c520f2b8d829be
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-11-26 23:46:51 -0500

    Add 'bats-support' and 'bats-assert' source directly
    * Use 'test/bootstrap.sh' to update the directories

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -3,4 +3,14 @@
 MYCELIO_ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" &>/dev/null && cd .. && pwd)"
 export MYCELIO_ROOT
 
-"$MYCELIO_ROOT/test/bats/bin/bats" --verbose-run "$MYCELIO_ROOT/test/test_error_handling.bats"
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-assert"
+git clone "https://github.com/ztombol/bats-assert" "${MYCELIO_ROOT}/test/test_helper/bats-assert"
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-assert/.git/"
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-assert/test/"
+
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-support"
+git clone "https://github.com/ztombol/bats-support" "${MYCELIO_ROOT}/test/test_helper/bats-support"
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-support/.git/"
+rm -rf "${MYCELIO_ROOT}/test/test_helper/bats-support/test/"
+
+bats --verbose-run "$MYCELIO_ROOT/test/test_error_handling.bats"
