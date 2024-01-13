a8997d63d4e3cab911b733f735316cbaabb70c47
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-13 12:10:56 -0800

    Move all profile batch functions into single function to consolidate functionality

--- source/windows/bin/profile.bat
+++ source/windows/bin/profile.bat
@@ -1,4 +1,5 @@
 @echo off
+goto:$Main
 ::
 :: This is used as an auto-run script and invoked for every new instance of 'cmd.exe' starts. This script
 :: is also invoked when running e.g. 'for /f %%l in ('some command') do ...'
@@ -16,7 +17,8 @@
 :: IMPORTANT: Do not use %CMDCMDLINE% as is may contain unprotected | & > < characters. Use !CMDCMDLINE! instead.
 ::
 
-setlocal EnableExtensions EnableDelayedExpansion
+:GetEnvironmentState
+    setlocal EnableExtensions EnableDelayedExpansion
     set MYCELIO_SKIP_INIT=0
 
     :: Change 'REM' to 'echo' to get output
@@ -30,7 +32,16 @@ setlocal EnableExtensions EnableDelayedExpansion
     set "CMD=!CMD:<=\x3C!"
     set "CMD=!CMD:&=\x36!"
 
-    call :SplitArgs !CMD!
+    ::-----------------------------------
+    :: Extract the ARG0 and ARG1 from %CMDCMDLINE% using cmd.exe own parser
+    ::-----------------------------------
+    goto:$GetEnvironmentStateSplitArgsEnd
+    :GetEnvironmentStateSplitArgs
+        set "ARG0=%1"
+        set "ARG1=%2"
+    exit /b
+    :$GetEnvironmentStateSplitArgsEnd
+    call :GetEnvironmentStateSplitArgs !CMD!
 
     :: for /f invokes %COMSPEC% without quotes, whereas new shells' ARG0 have quotes. If
     :: ARG0 equals COMSPEC then this is not a new top 'cmd.exe' instance.
@@ -47,15 +58,35 @@ setlocal EnableExtensions EnableDelayedExpansion
     goto:$InitializeProfile
 
     :$SkipInit
-    set MYCELIO_SKIP_INIT=1
-    set ECHO=REM
+    set "MYCELIO_SKIP_INIT=1"
+    goto:$InitializeProfile
 
     :$InitializeProfile
-    call :GetRoot "%~dp0..\..\..\" "%~dpnx0"
+    goto:$GetEnvironmentStateGetRoot
+    :GetEnvironmentStateGetRoot
+        if exist "%MYCELIO_ROOT%\setup.bat" exit /b 0
+        if "%MYCELIO_ROOT%"=="" (
+            set "MYCELIO_ROOT=%~dp1"
+            goto:$UpdateRoot
+        )
+
+        :$UpdateRoot
+        if "!MYCELIO_ROOT:~-1!"=="\" set "MYCELIO_ROOT=!MYCELIO_ROOT:~0,-1!"
+        set "ARG1=%2"
+    exit /b 0
+    :$GetEnvironmentStateGetRoot
+    call :GetEnvironmentStateGetRoot "%~dp0..\..\..\" "%~dpnx0"
+
+    set "ECHO=REM"
+    set "HOME=!HOME!"
+    if not exist "!HOME!" set "HOME=%USERPROFILE%"
+    if "!HOME!"=="" set "HOME=%USERPROFILE%"
+    if not exist "!HOME!" set "HOME=%USERPROFILE%"
+    set "MYCELIO_CLINK_SETTINGS=%LOCALAPPDATA%\clink"
 
     :: Generate and run the environment batch script
     set "MYCELIO_ENV_PATH=%~dp0env.bat"
-    if not exist "!_env!" (
+    if not exist "!MYCELIO_ENV_PATH!" (
         set "MYCELIO_ENV_PATH=!MYCELIO_ROOT!\source\windows\bin\profile.bat"
     )
 endlocal & (
@@ -65,101 +96,94 @@ endlocal & (
     set "MYCELIO_AUTORUN_INITIALIZED=1"
     set "MYCELIO_SKIP_INIT=%MYCELIO_SKIP_INIT%"
     set "MYCELIO_ECHO=%ECHO%"
+    set "MYCELIO_CLINK_SETTINGS=%MYCELIO_CLINK_SETTINGS%"
+    set "CLINK_NOAUTORUN=1"
 )
-if "%MYCELIO_SKIP_INIT%"=="1" goto:$InitializedProfile
-
-::
-:: This logo was generated with figlet after testing with selection of fonts.
-::
-::    - apt install figlet
-::    - git clone https://github.com/xero/figlet-fonts
-::    - find figlet-fonts/ -printf "%f\n" | xargs -n 1 -I % figlet -d ./figlet-fonts/ -f % myceli0
-::
-:: These fonts all display the logo quite well, see https://www.programmingfonts.org
-::
-::    - fire code (good but 'I' doesn't align)
-::    - gintronic (very nice)
-::    - hasklig (pretty good)
-::    - jetbrains mono (better than most)
-::    - julia-mono (amazing)
-::    - mensch
-::    - luculent
-::    - victor mono (quite good)
-::    - source code pro (bars have spaces)
-::
-
-:: Change to unicode
-if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
-%MYCELIO_ECHO% ▓├═════════════════════════════════
-%MYCELIO_ECHO% ▓│  ┏┏┓┓ ┳┏━┓┳━┓┳  o┏━┓
-%MYCELIO_ECHO% ▓│  ┃┃┃┗┏┛┃  ┣━ ┃  ┃┃/┃
-%MYCELIO_ECHO% ▓│  ┛ ┇ ┇ ┗━┛┻━┛┇━┛┇┛━┛
-%MYCELIO_ECHO% ▓├═════════════════════════════════
-
-:: Switch back to standard ANSI
-if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 1252 >NUL 2>&1
-
-if not exist "%MYCELIO_ENV_PATH%" goto:$InitializedProfile
-call "%MYCELIO_ENV_PATH%"
-
-:$InitializedProfile
-%MYCELIO_ECHO% [mycelio] Run `help` to get list of commands.
-
-:: Check to see if 'doskey' is valid first as some versions
-:: of Windows (e.g. nanoserver) do not have 'doskey' support.
-if "%USERNAME%"=="ContainerAdministrator" goto:$StartClink
-
-:: Some versions of Windows do not support using 'doskey' command
-:: so test it out before running all the commands.
-doskey /? >NUL 2>&1
-if errorlevel 1 goto:$SkipDosKeySetup
-    doskey cd.=cd /d "%MYCELIO_ROOT%"
-    doskey cd~ =cd /d "%HOME%"
-    doskey cp=copy $*
-    doskey mv=move $*
-    doskey h=doskey /HISTORY
-    doskey edit=%HOME%\.local\bin\micro.exe $*
-    doskey refresh=%MYCELIO_ROOT%\source\windows\bin\profile.bat --refresh
-    doskey where=@for %%E in (%PATHEXT%) do @for %%I in ($*%%E) do @if NOT "%%~$PATH:I"=="" echo %%~$PATH:I
-:$SkipDosKeySetup
-
-:: If we have already injected Clink then skip it
-if "%CLINK_INJECTED%"=="1" goto:$SkipClink
-    :: This must be the last operation we do.
-    call clink --version >NUL 2>&1
-    if errorlevel 1 (
-        %MYCELIO_ECHO% Initialized `dotfiles` environment without clink.
-        call :ClearErrorLevel
-    ) else (
-        set CLINK_INJECTED=1
-        call clink inject --session "dot_mycelio" --profile "%MYCELIO_ROOT%\source\windows\clink" --quiet --nolog
-    )
-:$SkipClink
-
-goto:$MycelioProfileEnd
-
-::-----------------------------------
-:: Extract the ARG0 and ARG1 from %CMDCMDLINE% using cmd.exe own parser
-::-----------------------------------
-:SplitArgs
-    set "ARG0=%1"
-    set "ARG1=%2"
-exit /b
-
-:GetRoot
-    if exist "%MYCELIO_ROOT%\setup.bat" exit /b 0
-    if "%MYCELIO_ROOT%"=="" (
-        set "MYCELIO_ROOT=%~dp1"
-        goto:$UpdateRoot
-    )
-
-    :$UpdateRoot
-    if "!MYCELIO_ROOT:~-1!"=="\" set "MYCELIO_ROOT=!MYCELIO_ROOT:~0,-1!"
-    set "ARG1=%2"
-exit /b
+exit /b 0
 
 :ClearErrorLevel
 exit /b 0
 
+:$Main
+    call :GetEnvironmentState
+    if "%MYCELIO_SKIP_INIT%"=="1" goto:$InitializedProfile
+
+    ::
+    :: This logo was generated with figlet after testing with selection of fonts.
+    ::
+    ::    - apt install figlet
+    ::    - git clone https://github.com/xero/figlet-fonts
+    ::    - find figlet-fonts/ -printf "%f\n" | xargs -n 1 -I % figlet -d ./figlet-fonts/ -f % myceli0
+    ::
+    :: These fonts all display the logo quite well, see https://www.programmingfonts.org
+    ::
+    ::    - fire code (good but 'I' doesn't align)
+    ::    - gintronic (very nice)
+    ::    - hasklig (pretty good)
+    ::    - jetbrains mono (better than most)
+    ::    - julia-mono (amazing)
+    ::    - mensch
+    ::    - luculent
+    ::    - victor mono (quite good)
+    ::    - source code pro (bars have spaces)
+    ::
+
+    :: Change to unicode
+    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
+    %MYCELIO_ECHO% ▓├╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝
+    %MYCELIO_ECHO% ▓│  ┝┝┓┓ ┳┝┝┓┳┝┓┳  o┝┝┓
+    %MYCELIO_ECHO% ▓│  ┃┃┃┗┝┛┃  ┣┝ ┃  ┃┃/┃
+    %MYCELIO_ECHO% ▓│  ┛ ┇ ┇ ┗┝┛┻┝┛┇┝┛┇┛┝┛
+    %MYCELIO_ECHO% ▓├╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝╝
+
+    :: Switch back to standard ANSI
+    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 1252 >NUL 2>&1
+
+    if not exist "%MYCELIO_ENV_PATH%" goto:$InitializedProfile
+    call "%MYCELIO_ENV_PATH%"
+
+    :$InitializedProfile
+    %MYCELIO_ECHO% [mycelio] Run `help` to get list of commands.
+
+    :: Check to see if 'doskey' is valid first as some versions
+    :: of Windows (e.g. nanoserver) do not have 'doskey' support.
+    if "%USERNAME%"=="ContainerAdministrator" goto:$StartClink
+
+    :: Some versions of Windows do not support using 'doskey' command
+    :: so test it out before running all the commands.
+    doskey /? >NUL 2>&1
+    if errorlevel 1 goto:$SkipDosKeySetup
+        doskey cd.=cd /d "%MYCELIO_ROOT%"
+        doskey cd~ =cd /d "%HOME%"
+        doskey cp=copy $*
+        doskey mv=move $*
+        doskey h=doskey /HISTORY
+        doskey which=where $*
+        doskey edit=%HOME%\.local\bin\micro.exe $*
+        doskey refresh=%MYCELIO_ROOT%\source\windows\bin\profile.bat --refresh
+        doskey where=@for %%E in (%PATHEXT%) do @for %%I in ($*%%E) do @if NOT "%%~$PATH:I"=="" echo %%~$PATH:I
+    :$SkipDosKeySetup
+
+    :: If we have already injected Clink then skip it
+    if "%CLINK_INJECTED%"=="1" goto:$ClinkDone
+        call clink --version >NUL 2>&1
+        if errorlevel 1 goto:$ClinkError
+
+        rem Injecting clink must be the last operation we do.
+        if not exist "%MYCELIO_CLINK_SETTINGS%" mkdir "%MYCELIO_CLINK_SETTINGS%"
+        if not exist "%HOME%\.local" mkdir "%HOME%\.local"
+        if not exist "%HOME%\.local\clink" mkdir "%HOME%\.local\clink"
+        set CLINK_INJECTED=1
+        call clink inject --session "dot_mycelio" --profile "%MYCELIO_CLINK_SETTINGS%" --scripts "%MYCELIO_ROOT%\source\windows\clink" --quiet --nolog
+
+    :$ClinkError
+    goto:$ClinkDone
+        %MYCELIO_ECHO% Initialized `dotfiles` environment without clink.
+        call :ClearErrorLevel
+        goto:$ClinkDone
+
+    :$ClinkDone
+
+    goto:$MycelioProfileEnd
 :$MycelioProfileEnd
 set MYCELIO_SKIP_INIT=
