cb6083a69aa526505105e708e203f3b13575f7f2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-04 19:13:16 -0800

    Remove 'Dockerfile.ubuntu.empy' and use 'Dockerfile.ubuntu' instead

--- test/windows/test_container_ubuntu.bat
+++ test/windows/test_container_ubuntu.bat
@@ -2,5 +2,5 @@
 
 call "%~dp0test_env.bat"
 
-docker build --progress plain --rm -t "ubuntu_empty" -f "%MYCELIO_ROOT%\source\docker\Dockerfile.ubuntu.empty" %MYCELIO_ROOT%
+docker build --progress plain --rm -t "ubuntu_empty" -f "%MYCELIO_ROOT%\source\docker\Dockerfile.ubuntu" %MYCELIO_ROOT%
 "%MYCELIO_ROOT%\source\windows\bin\cdbash.bat" run ubuntu_empty "bash"
