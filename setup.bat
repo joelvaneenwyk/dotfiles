d83ecf932b5c9cc0fe4e6b9be0471cd640e50d17
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-05 17:35:17 -0800

    fix: Addressed numerous path related issues in 'setup.bat'
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- setup.bat
+++ setup.bat
@@ -1,8 +1,232 @@
 @echo off
+goto:$Main
 
-if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
+::
+:: Local functions
+::
 
-setlocal EnableExtensions EnableDelayedExpansion
+:Run %*=Command with arguments
+    if "%GITHUB_ACTIONS%"=="" (
+        echo ##[cmd] %*
+    ) else (
+        echo [command]%*
+    )
+    call %*
+endlocal & (
+    set "MSYS_SHELL=%MSYS_SHELL%"
+    set "PATH=%PATH%"
+    exit /b %ERRORLEVEL%
+)
+
+:RunSudo %*=Command with arguments
+    if "%GITHUB_ACTIONS%"=="" (
+        echo ##[cmd] %*
+    ) else (
+        echo [command]%*
+    )
+    call sudo %*
+endlocal & (
+    set "MSYS_SHELL=%MSYS_SHELL%"
+    set "PATH=%PATH%"
+    exit /b %ERRORLEVEL%
+)
+
+:RunPowerShell %*=Command with arguments
+    setlocal EnableExtensions EnableDelayedExpansion
+
+    ::
+    :: Initialize each installed PowerShell we find
+    ::
+    set _powershell=
+    set _pwsh=^
+        "C:\Program Files\PowerShell\7\pwsh.exe"^
+        "C:\Program Files\PowerShell\pwsh.exe"^
+        "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+    for %%p in (!_pwsh!) do (
+        set _powershell=%%p
+        if exist !_powershell! goto:$PowerShellSet
+    )
+    :$PowerShellSet
+
+    if not exist "!_powershell!" (
+        echo ERROR: PowerShell not found: "!_powershell!" 1>&2
+        exit /b 1
+    )
+
+    :: By changing character page we prevent parent console from changing
+    :: font, see https://superuser.com/a/1548564
+    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 437 > nul
+    call :Run !_powershell! -NoLogo -NoProfile %*
+endlocal & (
+    set "MSYS_SHELL=%MSYS_SHELL%"
+    set "PATH=%PATH%"
+    exit /b %ERRORLEVEL%
+)
+
+:RunSudoPowerShell %*=Command with arguments
+    setlocal EnableExtensions EnableDelayedExpansion
+
+    ::
+    :: Initialize each installed PowerShell we find
+    ::
+    set "_powershell="
+    set _pwsh=^
+        "C:\Program Files\PowerShell\7\pwsh.exe"^
+        "C:\Program Files\PowerShell\pwsh.exe"^
+        "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+    for %%p in (!_pwsh!) do (
+        set "_powershell=%%p"
+        if exist !_powershell! goto:$PowerShellSet
+    )
+    :$PowerShellSet
+
+    if not exist "!_powershell!" (
+        echo ERROR: PowerShell not found: "!_powershell!" 1>&2
+        exit /b 1
+    )
+
+    :: By changing character page we prevent parent console from changing
+    :: font, see https://superuser.com/a/1548564
+    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 437 > nul
+    call :RunSudo !_powershell! -NoLogo -NoProfile %*
+endlocal & (
+    set "MSYS_SHELL=%MSYS_SHELL%"
+    set "PATH=%PATH%"
+    exit /b %ERRORLEVEL%
+)
+
+:GroupStart
+    if not "%GITHUB_ACTIONS%"=="" (
+        echo ::group::%~1
+    )
+endlocal & exit /b
+
+:GroupEnd
+    if not "%GITHUB_ACTIONS%"=="" (
+        echo ::endgroup::
+    )
+endlocal & exit /b %ERRORLEVEL%
+
+:Command
+setlocal EnableDelayedExpansion
+    set "_command=%*"
+    set "_command=!_command:   = !"
+    set "_command=!_command:  = !"
+    echo ##[cmd] !_command!
+    !_command!
+endlocal & exit /b %ERRORLEVEL%
+
+:CheckSystemFile %1=SystemFilename
+    setlocal EnableExtensions EnableDelayedExpansion
+
+    set _deploy="%MYCELIO_ROOT%\artifacts\windows"
+
+    if exist "%_deploy%\%~1" goto:$SystemDeploy
+
+    if not exist "%MYCELIO_ROOT%\artifacts" mkdir "%MYCELIO_ROOT%\artifacts"
+    if not exist "%_deploy%" mkdir "%_deploy%"
+
+    if exist "C:\Windows\System32\%~1" (
+        copy /B /Y /V "C:\Windows\System32\%~1" "%_deploy%\%~1" > nul 2>&1
+        echo Copied system file for Docker: '%~1'
+    )
+
+    :$SystemDeploy
+    :: Windows Nano Server does not include Robocopy so copy our local version
+    :: to the currently running server if it exists.
+    if not exist "C:\Windows\System32\%~1" (
+        copy /B /Y /V "%_deploy%\%~1" "C:\Windows\System32\%~1" > nul 2>&1
+        echo Deployed file to 'C:\Windows\System32\' path: '%~1'
+    )
+endlocal & exit /b %ERRORLEVEL%
+
+::-----------------------------------
+:: Check if the user has system administrator rights. !ERRORLEVEL! 0=Yes; 5=No
+::-----------------------------------
+:IsAdmin
+    >NUL 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+exit /b %ERRORLEVEL%
+
+::-----------------------------------
+:: Query if autorun installed
+::-----------------------------------
+:CheckAutoRunInstalled %1=Hive %2=OutputVarName
+    setlocal EnableExtensions EnableDelayedExpansion
+    set "KEY=%~1\Software\Microsoft\Command Processor"
+    for /f "tokens=2,3*" %%a in ('reg query "!KEY!" /v AutoRun 2^>NUL ^| findstr AutoRun') do (
+        set "TYPE=%%a"
+        set "VALUE=%%b"
+        if "!TYPE!"=="REG_EXPAND_SZ" call set "VALUE=!VALUE!"
+    )
+endlocal & (
+    if not "%~2"=="" (set "%~2=%VALUE%")
+    exit /b %ERRORLEVEL%
+)
+
+:CheckAutoRun %1=Hive %2=VarName
+    setlocal EnableExtensions EnableDelayedExpansion
+    set _check_return_value=2
+
+    set "_hive=%~1"
+    set "_var_name=%~2"
+    set "_profile=!%~2!"
+    if "%SPROFILE%"=="!_profile!" (
+        echo [mycelio] Autorun 'dotfiles' already registered: "!_profile!"
+        set _check_return_value=0
+        goto:$CheckAutoRunDone
+    )
+
+    ::
+    :: C:\Users\{USERNAME}\.dotfiles\source\windows\bin\profile.bat&"C:\Program Files (x86)\clink\clink.bat" inject --autorun
+    ::
+    echo Variables: !_var_name! - !_profile! - %SPROFILE%
+    echo [mycelio] WARNING: Different AutoRun script already installed for %_hive%: "!_profile!"
+
+    :: Delete the key otherwise next will display an error
+    set "KEY=%_hive%\Software\Microsoft\Command Processor"
+    call :Command reg delete "!KEY!" /v "AutoRun" /f
+    if "!ERRORLEVEL!"=="0" (
+        echo [mycelio] Removed existing key: "!KEY!\AutoRun"
+        set _check_return_value=1
+        goto:$CheckAutoRunDone
+    )
+    set _check_return_value=!ERRORLEVEL!
+
+    :$CheckAutoRunDone
+endlocal & (exit /b %_check_return_value%)
+
+::-----------------------------------
+:: Remove existing auto run and replace it if possible
+::-----------------------------------
+:InstallAutoRun
+    setlocal EnableExtensions EnableDelayedExpansion
+    echo [mycelio] Checking AutoRun registry key.
+    for %%h in (HKCU HKLM) do (
+        call :CheckAutoRunInstalled "%%h" "VarAutoRun"
+        call :CheckAutoRun "%%h" "VarAutoRun"
+        if "!ERRORLEVEL!"=="0" goto:$InstallAutoRunComplete
+    )
+
+    :# No keys should exist now so try to install
+    if not defined HIVE (
+        call :IsAdmin
+        if not errorlevel 1 (       :# Admin user. All user install.
+            set "HIVE=HKLM"
+        ) else (                    :# Normal user. Current user install.
+            set "HIVE=HKCU"
+        )
+    )
+
+    set "KEY=%HIVE%\Software\Microsoft\Command Processor"
+    %EXEC% reg add "%KEY%" /v "AutoRun" /t REG_SZ /d "%SPROFILE%" /f
+    echo Created registry key "%KEY%" value "AutoRun"
+    :$InstallAutoRunComplete
+endlocal & (exit /b 0)
+
+:$Main
+setlocal EnableDelayedExpansion
+    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
+
     set "_mycelio_root=%~dp0"
     set "_starting_directory=%cd%"
 
@@ -161,7 +385,10 @@ setlocal EnableExtensions EnableDelayedExpansion
 
     :: Initialize 'msys2' ("Minimal System") environment with bash script. We call the shim directly because environment
     :: may not read path properly after it has just been installed.
-    if not exist "%MSYS_SHELL%" (
+    if not defined MSYS_SHELL set "MSYS_SHELL=%USERPROFILE%\.local\msys64\msys2_shell.cmd"
+    if not exist "!MSYS_SHELL!" set "MSYS_SHELL=%USERPROFILE%\.local\msys64\msys2_shell.cmd"
+
+    if not exist "!MSYS_SHELL!" (
         set _error=55
         echo ERROR: MSYS2 not installed. Initialization failed. 1>&2
         goto:$InitializeDone
@@ -169,9 +396,9 @@ setlocal EnableExtensions EnableDelayedExpansion
 
     :: We intentionally use MINGW64 here because binaries that we compile (e.g., golang) need
     :: to be able to run without the MSYS dynamic libraries.
-    call :Run "%MSYS_SHELL%" -mingw64 -defterm -no-start -where "%_mycelio_root%" -shell bash -c "./setup.sh --home /c/Users/%USERNAME% !_args!"
-    if not "!ERRORLEVEL!"=="0" (
-        set _error=!ERRORLEVEL!
+    call :Run "!MSYS_SHELL!" -mingw64 -defterm -no-start -where "%_mycelio_root%" -shell bash -c "./setup.sh --home /c/Users/%USERNAME% !_args!"
+    if not "%ERRORLEVEL%"=="0" (
+        set "_error=%ERRORLEVEL%"
         echo ERROR: Shell setup with 'bash' failed. 1>&2
         goto:$InitializeDone
     )
@@ -184,218 +411,10 @@ endlocal & (
     set "MYCELIO_ERROR=%_error%"
     set "PATH=%PATH%"
     set "POWERSHELL=%_pwsh%"
-)
-
-if "%MYCELIO_ERROR%"=="0" (
-    echo Completed execution of `dotfiles` initialization.
-) else (
-    echo Execution of `dotfiles` initialization failed. Error code: '%MYCELIO_ERROR%' 1>&2
-)
-
-exit /b %MYCELIO_ERROR%
-
-::
-:: Local functions
-::
-
-:Run %*=Command with arguments
-    if "%GITHUB_ACTIONS%"=="" (
-        echo ##[cmd] %*
+    if "%MYCELIO_ERROR%"=="0" (
+        echo Completed execution of `dotfiles` initialization.
     ) else (
-        echo [command]%*
+        echo Execution of `dotfiles` initialization failed. Error code: '%MYCELIO_ERROR%' 1>&2
     )
-    call %*
-endlocal & exit /b
-
-:RunSudo %*=Command with arguments
-    if "%GITHUB_ACTIONS%"=="" (
-        echo ##[cmd] %*
-    ) else (
-        echo [command]%*
-    )
-    call sudo %*
-endlocal & exit /b
-
-:RunPowerShell %*=Command with arguments
-    setlocal EnableExtensions EnableDelayedExpansion
-
-    ::
-    :: Initialize each installed PowerShell we find
-    ::
-    set _powershell=
-    set _pwshs=^
-        "C:\Program Files\PowerShell\7\pwsh.exe"^
-        "C:\Program Files\PowerShell\pwsh.exe"^
-        "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-    for %%p in (!_pwshs!) do (
-        set _powershell=%%p
-        if exist !_powershell! goto:$PowerShellSet
-    )
-    :$PowerShellSet
-
-    if not exist "!_powershell!" (
-        echo ERROR: PowerShell not found: "!_powershell!" 1>&2
-        exit /b 1
-    )
-
-    :: By changing character page we prevent parent console from changing
-    :: font, see https://superuser.com/a/1548564
-    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 437 > nul
-    call :Run !_powershell! -NoLogo -NoProfile %*
-endlocal & exit /b
-
-:RunSudoPowerShell %*=Command with arguments
-    setlocal EnableExtensions EnableDelayedExpansion
-
-    ::
-    :: Initialize each installed PowerShell we find
-    ::
-    set _powershell=
-    set _pwshs=^
-        "C:\Program Files\PowerShell\7\pwsh.exe"^
-        "C:\Program Files\PowerShell\pwsh.exe"^
-        "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-    for %%p in (!_pwshs!) do (
-        set _powershell=%%p
-        if exist !_powershell! goto:$PowerShellSet
-    )
-    :$PowerShellSet
-
-    if not exist "!_powershell!" (
-        echo ERROR: PowerShell not found: "!_powershell!" 1>&2
-        exit /b 1
-    )
-
-    :: By changing character page we prevent parent console from changing
-    :: font, see https://superuser.com/a/1548564
-    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 437 > nul
-    call :RunSudo !_powershell! -NoLogo -NoProfile %*
-endlocal & exit /b
-
-:GroupStart
-    if not "%GITHUB_ACTIONS%"=="" (
-        echo ::group::%~1
-    )
-endlocal & exit /b
-
-:GroupEnd
-    if not "%GITHUB_ACTIONS%"=="" (
-        echo ::endgroup::
-    )
-endlocal & exit /b
-
-:Command
-setlocal EnableDelayedExpansion
-    set "_command=%*"
-    set "_command=!_command:   = !"
-    set "_command=!_command:  = !"
-    echo ##[cmd] !_command!
-    !_command!
-exit /b
-
-:CheckSystemFile %1=SystemFilename
-    setlocal EnableExtensions EnableDelayedExpansion
-
-    set _deploy="%MYCELIO_ROOT%\artifacts\windows"
-
-    if exist "%_deploy%\%~1" goto:$SystemDeploy
-
-    if not exist "%MYCELIO_ROOT%\artifacts" mkdir "%MYCELIO_ROOT%\artifacts"
-    if not exist "%_deploy%" mkdir "%_deploy%"
-
-    if exist "C:\Windows\System32\%~1" (
-        copy /B /Y /V "C:\Windows\System32\%~1" "%_deploy%\%~1" > nul 2>&1
-        echo Copied system file for Docker: '%~1'
-    )
-
-    :$SystemDeploy
-    :: Windows Nano Server does not include Robocopy so copy our local version
-    :: to the currently running server if it exists.
-    if not exist "C:\Windows\System32\%~1" (
-        copy /B /Y /V "%_deploy%\%~1" "C:\Windows\System32\%~1" > nul 2>&1
-        echo Deployed file to 'C:\Windows\System32\' path: '%~1'
-    )
-endlocal & exit /b
-
-::-----------------------------------
-:: Check if the user has system administrator rights. !ERRORLEVEL! 0=Yes; 5=No
-::-----------------------------------
-:IsAdmin
-    >NUL 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-exit /b
-
-::-----------------------------------
-:: Query if autorun installed
-::-----------------------------------
-:CheckAutoRunInstalled %1=Hive %2=OutputVarName
-    setlocal EnableExtensions EnableDelayedExpansion
-    set "KEY=%~1\Software\Microsoft\Command Processor"
-    for /f "tokens=2,3*" %%a in ('reg query "!KEY!" /v AutoRun 2^>NUL ^| findstr AutoRun') do (
-        set "TYPE=%%a"
-        set "VALUE=%%b"
-        if "!TYPE!"=="REG_EXPAND_SZ" call set "VALUE=!VALUE!"
-    )
-    endlocal & (
-        if not "%~2"=="" (set "%~2=%VALUE%")
-    )
-exit /b
-
-:CheckAutoRun %1=Hive %2=VarName
-    setlocal EnableExtensions EnableDelayedExpansion
-    set _check_return_value=2
-
-    set "_hive=%~1"
-    set "_var_name=%~2"
-    set "_profile=!%~2!"
-    if "%SPROFILE%"=="!_profile!" (
-        echo [mycelio] Autorun 'dotfiles' already registered: "!_profile!"
-        set _check_return_value=0
-        goto:$CheckAutoRunDone
-    )
-
-    ::
-    :: C:\Users\{USERNAME}\.dotfiles\source\windows\bin\profile.bat&"C:\Program Files (x86)\clink\clink.bat" inject --autorun
-    ::
-    echo Variables: !_var_name! - !_profile! - %SPROFILE%
-    echo [mycelio] WARNING: Different AutoRun script already installed for %_hive%: "!_profile!"
-
-    :: Delete the key otherwise next will display an error
-    set "KEY=%_hive%\Software\Microsoft\Command Processor"
-    call :Command reg delete "!KEY!" /v "AutoRun" /f
-    if "!ERRORLEVEL!"=="0" (
-        echo [mycelio] Removed existing key: "!KEY!\AutoRun"
-        set _check_return_value=1
-        goto:$CheckAutoRunDone
-    )
-    set _check_return_value=!ERRORLEVEL!
-
-    :$CheckAutoRunDone
-endlocal & (exit /b %_check_return_value%)
-
-::-----------------------------------
-:: Remove existing auto run and replace it if possible
-::-----------------------------------
-:InstallAutoRun
-    setlocal EnableExtensions EnableDelayedExpansion
-    echo [mycelio] Checking AutoRun registry key.
-    for %%h in (HKCU HKLM) do (
-        call :CheckAutoRunInstalled "%%h" "VarAutoRun"
-        call :CheckAutoRun "%%h" "VarAutoRun"
-        if "!ERRORLEVEL!"=="0" goto:$InstallAutoRunComplete
-    )
-
-    :# No keys should exist now so try to install
-    if not defined HIVE (
-        call :IsAdmin
-        if not errorlevel 1 (       :# Admin user. All user install.
-            set "HIVE=HKLM"
-        ) else (                    :# Normal user. Current user install.
-            set "HIVE=HKCU"
-        )
-    )
-
-    set "KEY=%HIVE%\Software\Microsoft\Command Processor"
-    %EXEC% reg add "%KEY%" /v "AutoRun" /t REG_SZ /d "%SPROFILE%" /f
-    echo Created registry key "%KEY%" value "AutoRun"
-    :$InstallAutoRunComplete
-endlocal & (exit /b 0)
+    exit /b %MYCELIO_ERROR%
+)
