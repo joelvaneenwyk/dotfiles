f02ce9bd9a4535fdbfe6af2e0e5718e6a5a6c0d5
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-11 18:13:05 -0700

    Significantly improved profile batch script
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- source/windows/bin/profile.bat
+++ source/windows/bin/profile.bat
@@ -1,29 +1,17 @@
-:: -*- coding: utf-8 -*-
-::
-:: This is used as an auto-run script and invoked for every new instance of 'cmd.exe' starts. This script
-:: is also invoked when running e.g. 'for /f %%l in ('some command') do ...'
-::
-:: In those cases we do not want to run the initialization as it can have unexpected side effects on the parent
-:: script variables. This severely affects the performance of scripts that heavily use for loops.
-::
-:: The command for running a new cmd instance is available in the "Command Prompt.lnk" shortcut and would be
-:: feasible to extract it from there using JScript WshShell.CreateShortcut() and then compare it to %CMDCMDLINE% variable
-:: but the performance penalty each time a cmd.exe starts would not be acceptable unless we do it only once
-:: at install time.
-::
-:: Workaround used here is that we check that a full quoted COMSPEC was used and _not_ using '/c' argument
-::
-:: IMPORTANT: Do not use %CMDCMDLINE% as is may contain unprotected | & > < characters. Use !CMDCMDLINE! instead.
-::
-@echo off
-goto:$Main
+@echo off && goto:$Main
+REM
+REM Mycelio - Profile Entrypoint
+REM
+REM This auto-run script runs for every new cmd executable instance and when
+REM running loops in control statements. We do not want it to run initialization
+REM steps in loops as it can have unexpected side effects on the parent script
+REM variables and can severely impact performance of scripts.
+REM
 
 :GetRoot
 setlocal EnableDelayedExpansion
     if exist "%MYCELIO_ROOT%\setup.bat" goto:$GetRootDone
-    if "%MYCELIO_ROOT%"=="" (
-        set "MYCELIO_ROOT=%~dp1"
-    )
+    if "%MYCELIO_ROOT%"=="" set "MYCELIO_ROOT=%~dp1"
 
     :$UpdateRoot
     if "!MYCELIO_ROOT:~-1!"=="\" set "MYCELIO_ROOT=!MYCELIO_ROOT:~0,-1!"
@@ -38,13 +26,37 @@ endlocal & (
 :ClearErrorLevel
 exit /b 0
 
+REM
+REM Although it is feasible to extract the command for running a new cmd instance
+REM from the Command Prompt "lnk" shortcut file using the "WshShell.CreateShortcut"
+REM function in JScript and then comparing it to the "CMDCMDLINE" variable but
+REM this would be performance heavy to do with each new instance which makes it
+REM unacceptable unless we do it only once at install time.
+REM
+REM The workaround used here is that we check that a fully quoted "COMSPEC" was
+REM used while avoiding the "/c" argument. Make sure to use delayed expansion
+REM for "CMDCMDLINE" as it may contain unprotected characters.
+REM
 :GetEnvironment
-setlocal EnableExtensions EnableDelayedExpansion
-    set MYCELIO_SKIP_INIT=0
+setlocal EnableDelayedExpansion EnableExtensions
+    goto:$GetEnvironmentStart
 
-    :: Change 'REM' to 'echo' to get output
-    set ECHO=echo
+    REM Local function we later call that extracts ARG0 and ARG1 from
+    REM the internal "CMDCMDLINE" variable using the cmd parser.
+    :SplitArgs
+        set "ARG0=%1"
+        set "ARG1=%2"
+    exit /b %errorlevel%
 
+    :$GetEnvironmentStart
+    set "MYCELIO_SKIP_INIT=0"
+
+    REM Change "REM" to "echo" to get output
+    set "ECHO=echo"
+
+    set "_clink=clink"
+    where !_clink! >NUL 2>&1
+    if errorlevel 1 set "_clink=%USERPROFILE%\scoop\shims\"
     if "%~1"=="--refresh" goto:$InitializeProfile
 
     set "CMD=!CMDCMDLINE!"
@@ -53,42 +65,37 @@ setlocal EnableExtensions EnableDelayedExpansion
     set "CMD=!CMD:<=\x3C!"
     set "CMD=!CMD:&=\x36!"
 
-    ::-----------------------------------
-    :: Extract the ARG0 and ARG1 from %CMDCMDLINE% using cmd.exe own parser
-    ::-----------------------------------
-    goto:$SplitArgs
-    :SplitArgs
-        set "ARG0=%1"
-        set "ARG1=%2"
-    exit /b %errorlevel%
-    :$SplitArgs
     call :SplitArgs !CMD!
 
-    :: for /f invokes %COMSPEC% without quotes, whereas new shells' ARG0 have quotes. If
-    :: ARG0 equals COMSPEC then this is not a new top 'cmd.exe' instance.
-    if "!ARG0!"=="%COMSPEC%" goto:$SkipInit
+    REM When using for loops it invokes COMSPEC without quotes whereas new
+    REM shells with ARG0 have quotes. If ARG0 equals COMSPEC then this is not
+    REM a new top level instance.
+    if "!ARG0!"=="%COMSPEC%" goto:$SkipProfileInitialization
 
-    :: This is not a new top cmd.exe instance
-    if /i "!ARG1!"=="/c" goto:$SkipInit
+    REM This is not a new top cmd.exe instance
+    if /i "!ARG1!"=="/c" goto:$SkipProfileInitialization
 
-    ::
-    :: This is a new top 'cmd.exe' instance so initialize it.
-    ::
-    if "%MYCELIO_AUTORUN_INITIALIZED%"=="1" goto:$SkipInit
-    if "%MYCELIO_PROFILE_INITIALIZED%"=="1" goto:$SkipInit
+    REM This is a new top cmd instance so initialize it.
+    if "%MYCELIO_AUTORUN_INITIALIZED%"=="1" goto:$SkipProfileInitialization
+    if "%MYCELIO_PROFILE_INITIALIZED%"=="1" goto:$SkipProfileInitialization
     goto:$InitializeProfile
 
-    :$SkipInit
-    set MYCELIO_SKIP_INIT=1
-    set ECHO=REM
+    :$SkipProfileInitialization
+    set "MYCELIO_SKIP_INIT=1"
+    set "ECHO=REM"
 
     :$InitializeProfile
     call :GetRoot "%~dp0..\..\..\" "%~dpnx0"
 
-    :: Generate and run the environment batch script
-    set "MYCELIO_ENV_PATH=%~dp0env.bat"
+    REM Generate and run the environment batch script
+    if not exist "!MYCELIO_ENV_PATH!" (
+        set "MYCELIO_ENV_PATH=%~dp0env.bat"
+    )
+    if not exist "!MYCELIO_ENV_PATH!" (
+        set "MYCELIO_ENV_PATH=!MYCELIO_ROOT!\source\windows\bin\env.bat"
+    )
     if not exist "!_env!" (
-        set "MYCELIO_ENV_PATH=!MYCELIO_ROOT!\source\windows\bin\profile.bat"
+        set "MYCELIO_ENV_PATH="
     )
 endlocal & (
     set "MYCELIO_ROOT=%MYCELIO_ROOT%"
@@ -98,54 +105,68 @@ endlocal & (
     set "MYCELIO_SKIP_INIT=%MYCELIO_SKIP_INIT%"
     set "MYCELIO_ECHO=%ECHO%"
     set "HOME=%USERPROFILE%"
-    exit /b %errorlevel%
 )
+exit /b %errorlevel%
 
+REM
+REM This logo was generated with figlet after testing with selection of fonts.
+REM
+REM    - apt install figlet
+REM    - git clone https://github.com/xero/figlet-fonts
+REM    - find figlet-fonts/ -printf "%f\n" ^| xargs -n 1 -I % figlet -d ./figlet-fonts/ -f % myceli0
+REM
+REM These fonts all display the logo quite well, see https://www.programmingfonts.org
+REM
+REM    - fire code (good but "I" does not align)
+REM    - gintronic (very nice)
+REM    - hasklig (pretty good)
+REM    - jetbrains mono (better than most)
+REM    - julia-mono (amazing)
+REM    - mensch
+REM    - luculent
+REM    - victor mono (quite good)
+REM    - source code pro (bars have spaces)
+REM
 :PrintLogo
-    ::
-    :: This logo was generated with figlet after testing with selection of fonts.
-    ::
-    ::    - apt install figlet
-    ::    - git clone https://github.com/xero/figlet-fonts
-    ::    - find figlet-fonts/ -printf "%f\n" | xargs -n 1 -I % figlet -d ./figlet-fonts/ -f % myceli0
-    ::
-    :: These fonts all display the logo quite well, see https://www.programmingfonts.org
-    ::
-    ::    - fire code (good but 'I' doesn't align)
-    ::    - gintronic (very nice)
-    ::    - hasklig (pretty good)
-    ::    - jetbrains mono (better than most)
-    ::    - julia-mono (amazing)
-    ::    - mensch
-    ::    - luculent
-    ::    - victor mono (quite good)
-    ::    - source code pro (bars have spaces)
-    ::
+    setlocal EnableDelayedExpansion
+    if "%MYCELIO_ECHO%"=="REM" goto:$PrintLogoDone
+    if "%MYCELIO_ECHO%"=="rem" goto:$PrintLogoDone
+    if "%MYCELIO_ECHO%"=="" goto:$PrintLogoDone
+    goto:$PrintLogoStart
 
-    :: Change to unicode
-    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
-    %MYCELIO_ECHO% ▓├═════════════════════════════════
-    %MYCELIO_ECHO% ▓│  ┏┏┓┓ ┳┏━┓┳━┓┳  o┏━┓
-    %MYCELIO_ECHO% ▓│  ┃┃┃┗┏┛┃  ┣━ ┃  ┃┃/┃
-    %MYCELIO_ECHO% ▓│  ┛ ┇ ┇ ┗━┛┻━┛┇━┛┇┛━┛
-    %MYCELIO_ECHO% ▓├═════════════════════════════════
+    :PrintStatement
+    call %MYCELIO_ECHO% %~1
+    exit /b 0
 
-    :: Switch back to standard ANSI
-    if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 1252 >NUL 2>&1
+    :$PrintLogoStart
+        REM Change to unicode
+        if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 65001 >NUL 2>&1
+        call :PrintStatement "▓├═════════════════════════════════"
+        call :PrintStatement "▓│  ┏┏┓┓ ┳┏━┓┳━┓┳  o┏━┓"
+        call :PrintStatement "▓│  ┃┃┃┗┏┛┃  ┣━ ┃  ┃┃/┃"
+        call :PrintStatement "▓│  ┛ ┇ ┇ ┗━┛┻━┛┇━┛┇┛━┛"
+        call :PrintStatement "▓├═════════════════════════════════"
+
+        REM Switch back to standard ANSI
+        if exist "C:\Windows\System32\chcp.com" call "C:\Windows\System32\chcp.com" 1252 >NUL 2>&1
+
+        goto:$PrintLogoDone
+
+    :$PrintLogoDone
 exit /b %errorlevel%
 
 :SetupDosKey
 setlocal
-    :: Some versions of Windows do not support using 'doskey' command
-    :: so test it out before running all the commands.
+    REM Some versions of Windows do not support using "doskey" command
+    REM so test it out before running all the commands.
     set "_doskey=C:\Windows\System32\doskey.exe"
     if not exist "%_doskey%" goto:$SkipDosKeySetup
 
-    :: Check to see if 'doskey' is valid first as some versions
-    :: of Windows (e.g. nanoserver) do not have 'doskey' support.
+    REM Check to see if "doskey" is valid first as some versions
+    REM of Windows like "nanoserver" do not have "doskey" support.
     if "%USERNAME%"=="ContainerAdministrator" goto:$SkipDosKeySetup
 
-    :: Make sure calling doskey works at all before we attempt to setup aliases
+    REM Make sure calling doskey works at all before we attempt to setup aliases
     call "%_doskey%" /? >NUL 2>&1
     if errorlevel 1 goto:$SkipDosKeySetup
         call "%_doskey%" cd.=cd /d "%MYCELIO_ROOT%"
@@ -162,17 +183,19 @@ exit /b %errorlevel%
 
 :$Main
     call :GetEnvironment
+    call :PrintLogo
+
     if "%MYCELIO_SKIP_INIT%"=="1" goto:$MainSkipInit
-        call :PrintLogo
-        if exist "%MYCELIO_ENV_PATH%" call "%MYCELIO_ENV_PATH%"
+    if not exist "%MYCELIO_ENV_PATH%" goto:$MainSkipInit
+    call "%MYCELIO_ENV_PATH%"
     :$MainSkipInit
 
     call :SetupDosKey
     %MYCELIO_ECHO% [mycelio] Run `help` to get list of commands.
 
-    :: If we have already injected Clink then skip it
+    REM If we have already injected Clink then skip it
     if "%CLINK_INJECTED%"=="1" goto:$SkipClink
-        :: This must be the last operation we do.
+        REM This must be the last operation we do.
         call clink --version >NUL 2>&1
         if errorlevel 1 (
             %MYCELIO_ECHO% Initialized `dotfiles` environment without clink.
@@ -185,4 +208,5 @@ exit /b %errorlevel%
 
     :$MycelioProfileEnd
     set "MYCELIO_SKIP_INIT="
+    set "MYCELIO_ECHO="
 goto:eof
