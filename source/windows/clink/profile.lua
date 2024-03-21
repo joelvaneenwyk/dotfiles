3aa1ca2fbb63c38a4bf127efd75da89c5891f31f
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-21 14:23:21 -0700

    Add a new 'logger' global to profile
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- source/windows/clink/profile.lua
+++ source/windows/clink/profile.lua
@@ -1,18 +1,62 @@
 --[[
-
+===============================================------
 Clink profile for 'dotfiles' project.
 
 See https://chrisant996.github.io/clink/clink.html for documentation. This is originally based
 on the Cmder boot script (see https://github.com/cmderdev/cmder/blob/master/config/cmder.lua) but has
 been somewhat heavily modified and also just out of date with most recent version.
+===============================================------
+--]]
 
-]]
+function mycelio_log(message, level)
+    local output_message = "[clink] "
+    local should_print_to_console = true
 
-local script_dir = path.normalise(debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]])
-local mycelio_root_dir = path.normalise(script_dir .. "../../..")
+    if level == nil then
+        level = 1
+    end
+
+    if level >= 4 then
+        output_message = output_message .. "debug "
+        if not ((settings ~= nil and settings.get("lua.debug")) or clink.DEBUG) then
+            should_print_to_console = false
+        end
+    elseif level == 3 then
+        output_message = output_message .. "info  "
+    elseif level == 2 then
+        output_message = output_message .. "warn  "
+    else
+        output_message = output_message .. "error "
+    end
+
+    output_message = output_message .. "| " .. message
+
+    if should_print_to_console then
+        print(output_message)
+        log.info(output_message)
+    end
+
+    return message
+end
+
+function mycelio_log_debug(message)
+    mycelio_log(message, 4)
+end
+
+function mycelio_log_info(message)
+    mycelio_log(message, 3)
+end
+
+function mycelio_log_warning(message)
+    mycelio_log(message, 3)
+end
+
+function mycelio_log_error(message)
+    mycelio_log(message, 3)
+end
 
 local color_normal = "\x1b[m"
-local settings = {
+local local_settings = {
     color_vsc_unknown = "\x1b[30;1m",
     color_vsc_clean = "\x1b[1;37;40m",
     color_vsc_dirty = "\x1b[31;1m",
@@ -23,31 +67,40 @@ local settings = {
     benchmark = false
 }
 
-local profile_settings = {extension_npm_cache = 1, extension_npm = 1}
+logger = {
+    debug = mycelio_log_debug,
+    info = mycelio_log_info,
+    warning = mycelio_log_warning,
+    error = mycelio_log_error
+}
 
 local function add_modules(input_path)
+    local profile_settings = {
+        extension_npm_cache = 1,
+        extension_npm = 1,
+    }
     local completions_dir = path.normalise(input_path)
-    print('[clink] Loading modules from path: "' .. completions_dir .. '"')
+    logger.debug('Loading modules from path: "' .. completions_dir .. '"')
     for _, lua_module in ipairs(clink.find_files(completions_dir .. '*.lua')) do
         -- Skip files that starts with _. This could be useful if some files should be ignored
 
-        if profile_settings["extension_" .. lua_module:match [[(.*).lua$]]] ~= -1 then
+        if profile_settings[ "extension_" .. lua_module:match [[(.*).lua$]] ] ~= -1 then
             if not string.match(lua_module, '^_.*') then
                 local filename = completions_dir .. lua_module
                 -- use dofile instead of require because require caches loaded modules
                 -- so config reloading using Alt-Q won't reload updated modules.
                 dofile(filename)
-                print('[clink] Module loaded: "' .. lua_module .. '"')
+                logger.debug('Module loaded: "' .. lua_module .. '"')
             end
         end
     end
-    print('[clink] Added all modules from path: "' .. completions_dir .. '"')
+    logger.info('Added all modules from path: "' .. completions_dir .. '"')
 end
 
 local cwd_prompt = clink.promptfilter(30)
 function cwd_prompt:filter(_)
     ---@diagnostic disable-next-line: undefined-field
-    return settings.color_prompt .. os.getcwd() .. color_normal
+    return local_settings.color_prompt .. os.getcwd() .. color_normal
 end
 
 -- A prompt filter that adds a line feed and angle bracket.
@@ -56,5 +109,11 @@ function bracket_prompt:filter(prompt)
     return prompt .. "\n → "
 end
 
-add_modules(mycelio_root_dir .. "/source/windows/clink-completions/")
-add_modules(mycelio_root_dir .. "/source/windows/clink/modules/")
+local function load_modules()
+    local script_dir = path.normalise(debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]])
+    local mycelio_root_dir = path.normalise(script_dir .. "../../..")
+    add_modules(mycelio_root_dir .. "/source/windows/clink-completions/")
+    add_modules(mycelio_root_dir .. "/source/windows/clink/modules/")
+end
+
+load_modules()
