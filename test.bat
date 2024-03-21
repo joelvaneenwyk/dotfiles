9870013c3ac9c2156d9ed616d7c8582780b7a908
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-21 14:22:53 -0700

    Improvements to test batch script
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- test.bat
+++ test.bat
@@ -1,3 +1,19 @@
-@echo off
-chcp 65001 1>nul
-luacheck . && busted
+@echo off && goto:$Main
+
+:$Main
+setlocal EnableDelayedExpansion
+    chcp 65001 1>nul
+
+    set luacheck="%USERPROFILE%\scoop\shims\luacheck.exe"
+    if not exist !luacheck! set "luacheck=luacheck"
+    !luacheck! .
+    if errorlevel 1 goto:$MainError
+
+    set busted="%USERPROFILE%\scoop\shims\busted.exe"
+    if not exist !busted! set "busted=busted"
+    !busted! .
+    if errorlevel 1 goto:$MainError
+
+    :$MainError
+    echo [ERROR] Lua tests failed.
+endlocal & exit /b 0
