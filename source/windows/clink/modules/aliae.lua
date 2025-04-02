feb0d7304d5ebe30f51aecf87f49df104e1193ea
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-04-01 20:11:39 -0700

    feat: refactor aliae initialization to use dynamic path resolution
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- source/windows/clink/modules/aliae.lua
+++ source/windows/clink/modules/aliae.lua
@@ -4,16 +4,21 @@
 -- https://aliae.dev/docs/
 --
 
-local file = io.popen('aliae init cmd')
+local aliae_path = "aliae"
+local potential_path = os.getenv("USERPROFILE") .. "\\AppData\\Local\\Programs\\aliae\\bin\\aliae.exe"
+aliae_path = potential_path
+
+local command = '\"' .. aliae_path .. '\"' .. ' init cmd'
+logger.info('##[cmd] ' .. command)
+load(io.popen(command):read("*a"))()
+
+local file = io.popen(command)
 local result = nil
 if file ~= nil then
-    local qresult = file:read('*a')
-    local ok = file:close()
-
-    if ok then
-        result = load(qresult)()
-        logger.info('Initialized aliae.')
-    end
+    result = file:read('*a')
+    load(result)()
+    file:close()
+    logger.info('Initialized aliae.')
 else
     logger.warning('Failed to setup aliae')
 end
